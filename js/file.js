68e211a9de46df5b60e869c955ede1dc41fc4db6
timi <tomdvazq@gmail.com>
2022-05-09 15:45:41 -0300

    mixins

--- js/file.js
+++ js/file.js
@@ -1,9 +1,9 @@
 let messageArray = ["> welcome to myCalculator!"];
 let textPosition = 0;
-let speed = 100;
+let speed = 150;
 
 typeWriter = () => {
-    document.querySelector("#text").innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+    document.querySelector("#text").innerHTML = messageArray[0].substring(0, textPosition) + '<span>\u25ae</span>';
 
     if(textPosition++ != messageArray[0].lenght)
         setTimeout(typeWriter, speed);
